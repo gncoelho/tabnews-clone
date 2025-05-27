f925a5b10aefbd4b2bb3607a65588938ec6e7609
Guilherme Coelho <gncoelho@users.noreply.github.com>
2025-05-27 15:23:04 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Ana, que tal uma sopa de beterraba na janta hoje?</h1>;
+  return (
+    <h1>
+      Ana, que tal uma sopa de beterraba na janta hoje? Ou que tal uma salada de
+      beterraba? =D
+    </h1>
+  );
 }
 
 export default Home;
